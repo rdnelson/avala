5e2f313e61dffd8a2899534506024824ed366c7a
Robert Nelson <robert@rnelson.ca>
2015-05-15 15:14:23 -0400

    Parse the website description

--- parse.go
+++ parse.go
@@ -1,35 +1,91 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"syscall"
+	"time"
 )
+
+type Heading struct {
+	Title, Url string
+}
+
+type Article struct {
+	Title   string
+	Date    time.Time
+	Content string
+}
+
+type Website struct {
+	PageTitle, Title, Subtitle string
+	Headings                   []Heading
+	Articles                   []Article
+}
+
+func progress(format string, a ...interface{}) {
+	fmt.Printf("  "+format+"\n", a...)
+}
 
 func parseRepo(repo, out string, bare bool) error {
 
 	os.MkdirAll(out, 0775)
 
+	site, err := handleSiteDescription(repo, out)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Printf("Processing website: %s\n", site.Title)
+
+	for i, head := range site.Headings {
+		progress("(%d/%d) Heading: %s -> %s", i+1, len(site.Headings), head.Title, head.Url)
+	}
+
+	println("Copying media files into place")
+
 	var count int
-	err := handleMedia(repo, "/media", out, &count)
+	err = handleMedia(repo, "/media", out, &count)
 
 	if err != nil {
 		switch err.(*os.PathError).Err.(syscall.Errno) {
 		case syscall.ENOENT:
-			println("No media directory found")
+			progress("No media directory found")
 			return nil
 		case syscall.EACCES:
-			println("Permission to media directory denied")
+			progress("Permission to media directory denied")
 		}
 
 		return err
 	}
 
-	println("Finished copying media files")
-
 	return nil
+}
+
+func handleSiteDescription(repo, out string) (*Website, error) {
+
+	file, err := os.Open(repo + "/website.json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	site := new(Website)
+
+	data, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &site)
+
+	return site, err
 }
 
 func handleMedia(repo, dir, out string, count *int) error {
@@ -48,13 +104,13 @@ func handleMedia(repo, dir, out string, count *int) error {
 	for i, file := range files {
 
 		if file.IsDir() {
-			fmt.Printf("(%d/%d) Copying: %s/\n", i+1+offset, *count, dir[1:]+"/"+file.Name())
-
 			var oldCount = *count
 			handleMedia(repo, dir+"/"+file.Name(), out, count)
 			offset += *count - oldCount
+
+			progress("(%d/%d) Copying: %s/", i+1+offset, *count, dir[1:]+"/"+file.Name())
 		} else {
-			fmt.Printf("(%d/%d) Copying: %s\n", i+1+offset, *count, dir[1:]+"/"+file.Name())
+			progress("(%d/%d) Copying: %s", i+1+offset, *count, dir[1:]+"/"+file.Name())
 			err = copyFileContents(repo+dir+"/"+file.Name(), out+dir+"/"+file.Name())
 		}
 
