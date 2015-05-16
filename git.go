7504ecca8aeabb5d02a65a3bfba4e2b02b8a4410
Robert Nelson <robert@rnelson.ca>
2015-05-15 23:58:25 -0400

    Added creation and modified date retreival functions

--- git.go
+++ git.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 func isBareRepo(repo string) (bool, error) {
@@ -12,4 +15,35 @@ func isBareRepo(repo string) (bool, error) {
 	}
 
 	return string(out) == "true", nil
+}
+
+func getCreatedDate(file string) time.Time {
+	return getActionTime(file, "A")
+}
+
+func getModifiedDate(file string) time.Time {
+	return getActionTime(file, "M")
+}
+
+func getActionTime(file, action string) time.Time {
+	if len(action) != 1 {
+		return time.Unix(0, 0)
+	}
+
+	cmd := exec.Command("git", "log", "-n1", "--format=%cI", "--diff-filter="+action, "--", file)
+	cmd.Dir = filepath.Dir(file)
+
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return time.Unix(0, 0)
+	}
+
+	date, err := time.Parse(time.RFC3339, strings.TrimSpace(string(out)))
+
+	if err != nil {
+		return time.Unix(0, 0)
+	}
+
+	return date
 }
