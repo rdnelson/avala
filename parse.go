c7503a0a85edc4aa9316bed51863be0d56279184
Robert Nelson <robert@rnelson.ca>
2015-05-19 17:56:53 -0400

    Added graceful failure for no website.json

--- parse.go
+++ parse.go
@@ -58,6 +58,11 @@ func parseRepo(repo, out string, bare bool) error {
 		return err
 	}
 
+	if site == nil {
+		println("No website.json found, cannot generate website.")
+		return nil
+	}
+
 	fmt.Printf("Processing website: %s\n", site.Title)
 
 	for i, head := range site.Headings {
@@ -118,6 +123,10 @@ func handleSiteDescription(repo, out string) (*Website, error) {
 	file, err := os.Open(filepath.Join(repo, "website.json"))
 
 	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+
 		return nil, err
 	}
 
