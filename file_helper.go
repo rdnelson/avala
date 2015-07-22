db172c871e689ce575481a4d33a5750ef22ded0f
Robert Nelson <robert@rnelson.ca>
2015-07-21 23:58:51 -0400

    Added custom file globber

--- file_helper.go
+++ file_helper.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -51,4 +53,45 @@ func getPermalink(path string) string {
 
 func getIndexPath(out string, date time.Time) string {
 	return filepath.FromSlash(fmt.Sprintf("%s/%d/%d/index.html", out, date.Year(), date.Month()))
+}
+
+func getAllFiles(dir string) (files []string, err error) {
+
+	files = []string{}
+
+	base := dir
+
+	if idx := strings.Index(dir, "*"); idx > -1 {
+		base = dir[:idx]
+	}
+
+	dir = strings.Replace(dir, ".", "\\.", -1)
+	dir = strings.Replace(dir, "**", "<<wilddir>>", -1)
+	dir = strings.Replace(dir, "*", "[^"+string(os.PathSeparator)+"]*", -1)
+	dir = strings.Replace(dir, "<<wilddir>>", ".*", -1)
+	dir = "^" + dir + "$"
+
+	pattern, err := regexp.Compile(dir)
+
+	if err != nil {
+		warning("Regex pattern failed: %s", err)
+		return
+	}
+
+	err = filepath.Walk(base, func(path string, info os.FileInfo, e error) error {
+		if e != nil {
+			warning("Walking '%s' failed: %s", path, e)
+			return nil
+		}
+
+		if !info.IsDir() {
+			if pattern.MatchString(path) {
+				files = append(files, path)
+			}
+		}
+
+		return nil
+	})
+
+	return
 }
